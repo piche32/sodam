a796855427d6f51ffc8d765d2b6d33557e169304
piche32 <piche32@naver.com>
2020-05-15 17:23:39 +0900

    Player Move
    Jump & Reverse

---
+++ Hindrance/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] float jumpPower = 400.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        move();
+    }
+
+    private void move()
+    {
+        //앞으로 움직이기(수정: 카메라 움직임 만들 때 활성화 시켜주기)
+        //  gameObject.transform.Translate(new Vector3(speed, 0.0f, 0.0f));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPower);
+        }
+        if (collision.gameObject.tag == "Ceiling")
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.down * jumpPower);
+        }
+    }
+
+    public void jumpBtDown()
+    {
+        //점프 버튼 눌렀을 때 정지
+        gameObject.GetComponent<Rigidbody>().Sleep();
+    }
+
+    public void jumpBtUp()
+    {
+        //점프 버튼을 떼면 무조건 떨어짐.. 중력만 받아서 그런듯(수정??)
+        gameObject.GetComponent<Rigidbody>().WakeUp();
+    }
+
+    public void reverseBtDown()
+    {
+        Physics.gravity = Vector3.up * 9.8f;
+    }
+
+    public void reverseBtUp()
+    {
+        Physics.gravity = Vector3.down * 9.8f;
+    }
+}
