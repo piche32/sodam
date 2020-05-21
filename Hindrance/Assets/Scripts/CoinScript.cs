d4c5d041dd0a8814b046ade8a03b485bbfb35ac1
piche32 <piche32@naver.com>
2020-05-22 03:55:38 +0900

    Coin Item & Score
    Coin Item 및 Score UI 추가

---
+++ Hindrance/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    [SerializeField] float coinScore = 1.0f;
+    GameObject UImanager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UImanager = GameObject.Find("UIManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            UImanager.GetComponent<UIManagerScript>().Score += coinScore;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Hindrance/Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerScript : MonoBehaviour
+{
+    [SerializeField] Text scoreText;
+    float score; //다른 곳에서 score를 관리하면 없애주기
+    public float Score { get { return score; } set { score = value; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "SCORE: " + score.ToString();
+    }
+   
+}
