d04e86182f94327ae526b69147ee31c45bf5f872
KDB1020 <wlsrpr@gmail.com>
2020-05-23 02:21:10 +0900

    방해물 수정
    제가 무엇을 잘못건든건지... 플레이어 스탑키가 정상적으로 작동이 되지 않습니다..
    + 방해물 스탑키 수정
    + 방해물 리버스키 구현

---
+++ Hindrance/Assets/Scripts/ReverseEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class ReverseEnemy : MonoBehaviour
+{
+    public bool isTop = true;  // true면 천장, flase면 바닥에 위치
+
+    private float gr = 980.0f;  // 중력값
+
+    public void BtDownReverse()
+    {
+        if (isTop)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.down * gr);
+        }
+        if (!isTop)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * gr);
+        }
+    }
+
+    public void BtUpReverse()
+    {
+        if (isTop)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * gr);
+        }
+        if (!isTop)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.down * gr);
+        }
+    }
+}
