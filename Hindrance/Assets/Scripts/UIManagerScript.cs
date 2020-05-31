f335b7abad371f00910cd21acd77e4ca0b9746c5
piche32 <piche32@naver.com>
2020-06-01 02:32:50 +0900

    Menu UI
    Menu랑 setting UI 추가

--- Hindrance/Assets/Scripts/UIManagerScript.cs
+++ Hindrance/Assets/Scripts/UIManagerScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManagerScript : MonoBehaviour
 {
@@ -9,16 +10,28 @@ public class UIManagerScript : MonoBehaviour
     [SerializeField] Text HPText;
 
     [SerializeField] GameObject option = null;
-    [SerializeField] GameObject tutorial = null;
+    [SerializeField] GameObject setting = null;
     float score; //다른 곳에서 score를 관리하면 없애주기
     public float Score { get { return score; } set { score = value; } }
 
     private static bool isPause;
     public static bool IsPause { get { return isPause; } set { isPause = value; } }
+
+    [SerializeField] AudioSource bgm = null;
+    Toggle bgmToggle;
+
+    [SerializeField] AudioSource effectSound = null;
+    Toggle effectSoundToggle = null;
+
+    [SerializeField]AudioClip openSound = null;
+    [SerializeField]AudioClip closeSound = null;
+
     // Start is called before the first frame update
     void Start()
     {
         score = 0.0f;
+        bgmToggle = null;
+        effectSoundToggle = null;
     }
 
     // Update is called once per frame
@@ -44,9 +57,11 @@ public class UIManagerScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (!option.activeSelf) {
+            if (!option.activeSelf&&!setting.activeSelf) {
                 isPause = true;
                 option.SetActive(true);
+                effectSound.clip = openSound;
+                effectSound.Play();
             }
         }
         return;
@@ -58,7 +73,54 @@ public class UIManagerScript : MonoBehaviour
         {
             isPause = false;
             option.SetActive(false);
+            effectSound.clip = closeSound;
+            effectSound.Play();
         }
     }
 
+
+    public void settingOpen()
+    {
+        option.SetActive(false);
+        setting.SetActive(true);
+        bgmToggle = GameObject.Find("bgmToggle").GetComponent<Toggle>();
+        effectSoundToggle = GameObject.Find("EffectSoundToggle").GetComponent<Toggle>();
+        effectSound.clip = openSound;
+        effectSound.Play();
+        return;
+    }
+
+    public void settingClose()
+    {
+        setting.SetActive(false);
+        isPause = false;
+        effectSound.clip = closeSound;
+        effectSound.Play();
+    }
+
+    public void restartBT()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    public void quitBT()
+    {
+        SceneManager.LoadScene("Start"); //나중에 바꿔주기
+    }
+
+    public void bgmCtrl()
+    {
+        if (bgm == null) return;
+        if (bgmToggle == null) return;
+        if (bgmToggle.isOn) bgm.Play();
+        else bgm.Stop();
+    }
+
+    public void effectSoundCtrl()
+    {
+        if (effectSound == null) return;
+        if (effectSoundToggle == null) return;
+        if (effectSoundToggle.isOn) effectSound.mute = false;
+        else effectSound.mute = true;
+    }
 }
