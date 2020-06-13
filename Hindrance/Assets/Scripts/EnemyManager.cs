62c8f51ce42575ccbeb0f6eec2b2a59774ffb4cf
KDB1020 <wlsrpr@gmail.com>
2020-06-13 18:58:57 +0900

    임시(2)
    수정할것: (1) reverseEnemy가 reverse버튼을 누르는 도중에 떼면 그 자리에서 멈추는 문제.
    (2)player가 stop키를 누른 상태면 적, 벽을 무시하는, 일종의 무적현상
    (3) Enemy 좌,우 이동이 맵 움직임으로 인해 나타나는 문제
    
    +Enemy 코드 수정
    +prefab 수정

--- Hindrance/Assets/Scripts/EnemyManager.cs
+++ Hindrance/Assets/Scripts/EnemyManager.cs
@@ -6,101 +6,33 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    static EnemyManager instance;
-    public static EnemyManager Call() { return instance; }                                 
+    public GameObject[] Enemy;
 
-    GameObject[] _StopEnemy;      // 프리팹 
-    public List<GameObject> StopEnemyList = new List<GameObject>();    // 객체 저장 리스트
-
-    GameObject[] _ReverseEnemy;
-    public List<GameObject> ReverseEnemyList = new List<GameObject>();
-
-    public int stopCount;   // stopEnemy
-
-    public int reverseCount;    // reverseEnemy
-
-    private void Awake()
+    public void DownStopButton()
     {
-        instance = this;
+        if (EnemyScript.Call().StopEnemyList == null) return; 
+      
+        EnemyScript.Call().StopBtDown();
+
+    }
+    public void UpStopButton()
+    {
+        if (EnemyScript.Call().StopEnemyList == null) return;
+
+        EnemyScript.Call().StopBtUp();
+
     }
 
-    private void OnDestroy()
+    public void DownReverseButton()
     {
-        MemoryDelete();
-        instance = null;
+        if (EnemyScript.Call().ReverseEnemyList == null) return;
+
+        EnemyScript.Call().ReverseBtDown();
     }
-
-    void Start()
+    public void UpReveseButton()
     {
-        StopEnemyList.Capacity = stopCount;
-        ReverseEnemyList.Capacity = reverseCount;
+        if (EnemyScript.Call().ReverseEnemyList == null) return;
 
-        FindEnemy(stopCount, reverseCount);
-    }
-
-    public void FindEnemy(int s_count, int r_count)
-    {
-        for (int i = 0; i < s_count; i++)      // 해당 적 찾아서 리스트 저장
-        {
-            _StopEnemy = GameObject.FindGameObjectsWithTag("StopEnemy");
-            StopEnemyList.Add(_StopEnemy[i]);
-        }
-
-        for (int i = 0; i < r_count; i++)
-        {
-            _ReverseEnemy = GameObject.FindGameObjectsWithTag("ReverseEnemy");
-            ReverseEnemyList.Add(_ReverseEnemy[i]);
-        }
-    }
-
-    public void StopBtDown()    
-    {
-        foreach (GameObject enemy in StopEnemyList)
-        {
-            enemy.gameObject.GetComponent<MoveEnemy>().EnemySpeed = 0;
-        }
-    }
-    public void StopBtUp()
-    {
-        foreach (GameObject enemy in StopEnemyList)
-        {
-            enemy.gameObject.GetComponent<MoveEnemy>().EnemySpeed = enemy.gameObject.GetComponent<MoveEnemy>().tmp;
-        }
-    }
-
-    public void ReverseBtDown()
-    {
-        foreach(GameObject enemy in ReverseEnemyList)
-        {
-            enemy.gameObject.GetComponent<ReverseEnemy>().BtDownReverse();
-        }
-    }
-    public void ReveseBtUp()
-    {
-        foreach(GameObject enemy in ReverseEnemyList)
-        {
-            enemy.gameObject.GetComponent<ReverseEnemy>().BtUpReverse();
-        }
-    }
-
-    public void MemoryDelete()  // 메모리 삭제 
-    {
-        if (StopEnemyList == null) return;
-
-        for (int i = 0; i < stopCount; i++)
-        {
-            GameObject obj = StopEnemyList[i];
-            GameObject.Destroy(obj);
-        }
-        StopEnemyList = null;
-
-        if (ReverseEnemyList == null) return;
-
-        for (int i = 0; i < reverseCount; i++)
-        {
-            GameObject obj = ReverseEnemyList[i];
-            GameObject.Destroy(obj);
-        }
-        ReverseEnemyList = null;
+        EnemyScript.Call().ReveseBtUp();
     }
 }
---
+++ Hindrance/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    static EnemyScript instance;
+    public static EnemyScript Call() { return instance; }
+
+    GameObject[] _StopEnemy;      // 프리팹 
+    public List<GameObject> StopEnemyList = new List<GameObject>();    // 객체 저장 리스트
+
+    GameObject[] _ReverseEnemy;
+    public List<GameObject> ReverseEnemyList = new List<GameObject>();
+
+    public int stopCount;   // stopEnemy
+
+    public int reverseCount;    // reverseEnemy
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        MemoryDelete();
+        instance = null;
+    }
+
+    void Start()
+    {
+        StopEnemyList.Capacity = stopCount;
+        ReverseEnemyList.Capacity = reverseCount;
+
+        FindEnemy(stopCount, reverseCount);
+    }
+
+    public void FindEnemy(int s_count, int r_count)
+    {
+        for (int i = 0; i < s_count; i++)      // 해당 적 찾아서 리스트 저장
+        {
+            _StopEnemy = GameObject.FindGameObjectsWithTag("StopEnemy");
+            StopEnemyList.Add(_StopEnemy[i]);
+        }
+
+        for (int i = 0; i < r_count; i++)
+        {
+            _ReverseEnemy = GameObject.FindGameObjectsWithTag("ReverseEnemy");
+            ReverseEnemyList.Add(_ReverseEnemy[i]);
+        }
+    }
+
+    public void StopBtDown()
+    {
+        foreach (GameObject enemy in StopEnemyList)
+        {
+            enemy.gameObject.GetComponent<MoveEnemy>().EnemySpeed = 0;
+        }
+    }
+    public void StopBtUp()
+    {
+        foreach (GameObject enemy in StopEnemyList)
+        {
+            enemy.gameObject.GetComponent<MoveEnemy>().EnemySpeed = enemy.gameObject.GetComponent<MoveEnemy>().tmp;
+        }
+    }
+
+    public void ReverseBtDown()
+    {
+        foreach (GameObject enemy in ReverseEnemyList)
+        {
+            enemy.gameObject.GetComponent<ReverseEnemy>().BtDownReverse();
+        }
+    }
+    public void ReveseBtUp()
+    {
+        foreach (GameObject enemy in ReverseEnemyList)
+        {
+            enemy.gameObject.GetComponent<ReverseEnemy>().BtUpReverse();
+        }
+    }
+
+    public void MemoryDelete()  // 메모리 삭제 
+    {
+        if (StopEnemyList == null) return;
+
+        for (int i = 0; i < stopCount; i++)
+        {
+            GameObject obj = StopEnemyList[i];
+            GameObject.Destroy(obj);
+        }
+        StopEnemyList = null;
+
+        if (ReverseEnemyList == null) return;
+
+        for (int i = 0; i < reverseCount; i++)
+        {
+            GameObject obj = ReverseEnemyList[i];
+            GameObject.Destroy(obj);
+        }
+        ReverseEnemyList = null;
+    }
+}
